b426494db5ee666511fe717549c75647993cb086
Thanapat1502 <psy150201027@gmail.com>
2024-11-15 15:10:50 +0700

    feat: setting connection

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,12 +1,30 @@
 import { Router } from "express";
+import { db } from "../utils/db";
 
 const productRouter = Router();
+const collection = db.collection("products");
 
 productRouter.get("/", (req, res) => {});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const newPost = { ...req.body };
+  try {
+    const result = await collection.insertOne({
+      name: newPost.name,
+      price: newPost.price,
+      image: newPost.image,
+      description: newPost.description,
+      category: newPost.category,
+    });
+    return res.status(201).json({
+      message: `Product has been created successfully : ${result} `,
+    });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
 
 productRouter.put("/:id", (req, res) => {});
 
